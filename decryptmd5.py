3036dea88e314f0cb002765d9d6d33dbe2a7e771
D-captainkenya <43841870+d-captainkenya@users.noreply.github.com>
2021-10-18 08:29:26 +0000

    Update decryptmd5.py

--- decryptmd5.py
+++ decryptmd5.py
@@ -1,29 +1,59 @@
+import pyfiglet         # Disclaimer: This script is for educational purposes only.
+import sys                              
 import hashlib
 
-flag = 0
+def disp():
+    print("\n"+"*"*65) 
+    print(pyfiglet.figlet_format("          Dcap-Md5"))
+    print("*\t\t\tD_captainkenya\t\t\t\t*")
+    print("*\t\t    Dcaptainkenya@gmail.com\t\t\t*")
+    print("*\t      https://www.D-captainkenya.github.io\t\t*")
+    print("*"*65)
+    print("\nDecrypt MD5 Hashes with wordlist")
+    print("\n"+"-"*50)
+    args()
 
-pass_hash = input("Enter an md5 hash: ")
-wordlist  = input("Enter wordlist file name: ")
+def usage():
+    print("\nplease specify md5hash and wordlist correctly!")
+    print("\nUSAGE:")
+    print("\tpython crackmd5hash.py md5hash wordlistpath")
+    print("EXAMPLE\n\tPython crackmd5hash.py ab334feeb31c05124cb73fa12571c2f6 /home/worldist.txt\n")
+    print("\tPython crackmd5hash.py ab334feeb31c05124cb73fa12571c2f6 D:\\myfiles\welcome.txt\n")
+    sys.exit(0)
 
-try:
-    pass_file = open (wordlist, "r")
-except:
-    print("No file found !!")
-    quit() 
+def args():
+    try:
+        n = len(sys.argv)
+        if n < 1:
+            usage()
+        elif n > 2:
+            work(sys.argv[1], sys.argv[2])
+        else:
+            usage()
+            sys.exit(0)
+    except KeyboardInterrupt:
+        print("\nKeyboard interrupted. \nExiting...")
+        sys.exit(0)
 
-for word in pass_file:
-    enc_wrd = word.encode('utf-8')
-    digest  = hashlib.md5(enc_wrd.strip()).hexdigest()
-
-    print(word)
-    print(digest)
-    print(pass_hash)
-
-    if digest == pass_hash:
-        print("Password Found")
-        print("The password is " +word)
-        flag = 1
-        break
-if flag == 0:
-    print("Password Not Found In The List, Try A Better List")
+def work(phash, wlist):
+    f = False
+    try:
+        passfile = open (wlist, "r")
+    except:
+        print("Wordlist Not Found !!")
+        usage()
+        sys.exit(0) 
+    print("working....")
+    for word in passfile:
+        enc_wrd = word.encode('utf-8')
+        digest  = hashlib.md5(enc_wrd.strip()).hexdigest()
+        if digest == phash:
+            print(f"\nFound A Match for '{digest}'")
+            print("The Password Is: " +word)
+            f = True
+            break
+    if f == False:
+        print("\nPassword Not Found In Wordlist,\nTry A Different List\n")
     
+if __name__ == "__main__":
+    disp()
